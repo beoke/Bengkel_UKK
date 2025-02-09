d699d01584b3172124dc41869cb4cec44924fcfb
Dhafa Bintang <sucsespart1@gmail.com>
2025-02-10 06:09:59 +0700

    form booking

--- Bengkel_UKK/Admin/Data/form_booking.Designer.cs
+++ Bengkel_UKK/Admin/Data/form_booking.Designer.cs
@@ -28,12 +28,156 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "form_booking";
+            panel1 = new Panel();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            comboBox1 = new ComboBox();
+            label5 = new Label();
+            comboBox2 = new ComboBox();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveBorder;
+            panel1.Controls.Add(comboBox2);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(comboBox1);
+            panel1.Controls.Add(dateTimePicker1);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(textBox4);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(textBox1);
+            panel1.Location = new Point(8, 5);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(522, 425);
+            panel1.TabIndex = 0;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(243, 15);
+            label4.Name = "label4";
+            label4.Size = new Size(66, 21);
+            label4.TabIndex = 7;
+            label4.Text = "Keluhan";
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox4.Location = new Point(243, 39);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(205, 105);
+            textBox4.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(15, 194);
+            label3.Name = "label3";
+            label3.Size = new Size(99, 21);
+            label3.TabIndex = 5;
+            label3.Text = "Paket service";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(15, 71);
+            label2.Name = "label2";
+            label2.Size = new Size(104, 21);
+            label2.TabIndex = 3;
+            label2.Text = "Booking Pada";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(15, 15);
+            label1.Name = "label1";
+            label1.Size = new Size(82, 21);
+            label1.TabIndex = 1;
+            label1.Text = "Pelanggan";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(19, 39);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(201, 29);
+            textBox1.TabIndex = 0;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            dateTimePicker1.Location = new Point(16, 95);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(204, 29);
+            dateTimePicker1.TabIndex = 8;
+            // 
+            // comboBox1
+            // 
+            comboBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(16, 162);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(204, 29);
+            comboBox1.TabIndex = 9;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(16, 138);
+            label5.Name = "label5";
+            label5.Size = new Size(187, 21);
+            label5.TabIndex = 10;
+            label5.Text = "Kendaraan Yang di servce";
+            // 
+            // comboBox2
+            // 
+            comboBox2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(14, 218);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(205, 29);
+            comboBox2.TabIndex = 11;
+            // 
+            // form_booking
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(541, 443);
+            Controls.Add(panel1);
+            Name = "form_booking";
+            Text = "form_booking";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private Label label4;
+        private TextBox textBox4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox1;
+        private ComboBox comboBox2;
+        private Label label5;
+        private ComboBox comboBox1;
+        private DateTimePicker dateTimePicker1;
     }
 }
